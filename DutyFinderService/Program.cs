52b3f0365368f547a8dd416c5975962ff94acc04
Tawmy <git@tawmy.dev>
2026-03-21 19:47:42 +0100

    Always map OpenAPI document

--- DutyFinderService/Program.cs
+++ DutyFinderService/Program.cs
@@ -6,11 +6,7 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
 
 var summaries = new[]
 {
